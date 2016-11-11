e1a91233679de0f0064c58c5701d089c0213a369
yurko <4au.4au.4au@gmail.com>
2016-11-11 18:08:15 +0200

    l

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,28 @@
+"use strict";
+
+var navClass = function(){
+	var pageCount = $('.slides > section > section').length;
+	var clearClass = function(){
+		for (var i=1; i<=pageCount; i++) {
+			$('.nav').removeClass('page_'+i);
+		}
+	}
+
+	var setNavClass = function(){
+		for (var i=1; i<=pageCount; i++) {
+			if ( $('html').hasClass(i) ) {
+				clearClass();
+				$('.nav').addClass('page_'+i);
+			}
+		}
+	}
+	setNavClass();	
+}
+
 $(window).load(function(){
-	
+	navClass();
+
+	Reveal.addEventListener( 'slidechanged', function( event ) {
+	    navClass();
+	});
 });

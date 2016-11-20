deaf5cf5118c35196603ce8eb1d831c53aee8cfd
Yurko Dmitriev <4au.4au.4au@gmail.com>
2016-11-20 17:36:34 +0200

    ваыпр

--- js/myscript.js
+++ js/myscript.js
@@ -1,42 +1,33 @@
 "use strict";
 
-/* add class to nav */
-var navClass = function(){
-	var pageCount = $('.slides > section > section').length;
-	var clearClass = function(){
-		for (var i=1; i<=pageCount; i++) {
-			$('.nav').removeClass('page_'+i);
-		}
+var slideChange = function(){
+	if ( $('#home__about').hasClass("present") ) {
+		$('.home__about__img').fadeIn();
+	} else {
+		$('.home__about__img').fadeOut();
 	}
 
-	var setNavClass = function(){
-		for (var i=1; i<=pageCount; i++) {
-			if ( $('html').hasClass(i) ) {
-				clearClass();
-				$('.nav').addClass('page_'+i);
-			}
-		}
-	}
-	setNavClass();	
 }
 
-/* set anchors */
-var navAnchors = function(){
-	$('.nav a:nth-child(1)').click(function(){
-		Reveal.slide( 1 );
-	});
-	$('.nav a:nth-child(2)').click(function(){
-		Reveal.slide( 1 );
-	});
-	$('.nav a:nth-child(3)').click(function(){
-		Reveal.slide( 1 );
+var navFix = function(){
+	$('.header a').click(function(){
+		var currentPage = $(this).attr('href');		
+		var hash = '#' + window.location.hash.substr(2);
+		
+		if (currentPage == hash) {
+			return false;
+		}
+
 	});
 }
+
 
 $(window).load(function(){
-	navClass();
+	slideChange();
+	navFix();
 
 	Reveal.addEventListener( 'slidechanged', function( event ) {
-	    navClass();
+		slideChange();
 	});
+	
 });

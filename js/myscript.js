910158878039634d1241da42cc98d3f4c7148530
yurko <4au.4au.4au@gmail.com>
2016-11-22 17:59:06 +0200

    ;;

--- js/myscript.js
+++ js/myscript.js
@@ -12,22 +12,59 @@ var slideChange = function(){
 var navFix = function(){
 	$('.header a').click(function(){
 		var currentPage = $(this).attr('href');		
-		var hash = '#' + window.location.hash.substr(2);
-		
+		var hash = '#' + window.location.hash.substr(2);		
 		if (currentPage == hash) {
 			return false;
 		}
-
 	});
 }
 
 var sliderParallax = function(){
+
 	$('.owl-next').click(function(){
-		var el = $('.reveal .home__projects .slide .table-cell:first-child img');
-		el.addClass( "act" ).one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
-            el.removeClass( "act" );
+
+		var currentIndex = $('.owl-item.active').index();
+		var totalItems = $('.owl-item').length - 1;
+
+		$('.reveal .home__projects .owl-item:eq('+currentIndex+') .slide .table-cell:first-child img').addClass('act');
+
+		var prevSlide = currentIndex - 1;
+		if (prevSlide < 0) {
+			prevSlide = 2;
+		}
+
+		var prevSlideElem = $('.reveal .home__projects .owl-item:eq('+prevSlide+') .slide .table-cell:first-child img');
+		prevSlideElem.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+            prevSlideElem.removeClass( "act" );
         });
+
 	});
+
+	$('.owl-prev').click(function(){
+
+		var currentIndex = $('.owl-item.active').index();
+		var totalItems = $('.owl-item').length - 1;
+
+		$('.reveal .home__projects .owl-item:eq('+currentIndex+') .slide .table-cell:first-child img').addClass('act');
+
+		var prevSlide = currentIndex + 1;
+		if (prevSlide > 2) {
+			prevSlide = 0;
+		}
+
+		var prevSlideElem = $('.reveal .home__projects .owl-item:eq('+prevSlide+') .slide .table-cell:first-child img');
+		prevSlideElem.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+            prevSlideElem.removeClass( "act" );
+        });
+
+	});
+}
+
+var sliderParallaxOnScroll = function(){
+	if ( $('#home__projects').hasClass("present") ) {
+		var currentIndex = $('.owl-item.active').index();
+		console.log(currentIndex);
+	}
 }
 
 
@@ -38,6 +75,7 @@ $(window).load(function(){
 
 	Reveal.addEventListener( 'slidechanged', function( event ) {
 		slideChange();
+		sliderParallaxOnScroll();
 	});
 	
 });

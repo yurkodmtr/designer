f50c3d51ea5153b03379d0bd207498df5c354229
Yurko Dmitriev <4au.4au.4au@gmail.com>
2016-11-27 16:37:01 +0200

    bb

--- js/myscript.js
+++ js/myscript.js
@@ -30,24 +30,17 @@ var sliderParallaxOnload = function(){
 
 var breakpoint = function(){
 	var w = $(window).width();
-	if ( w <=768 && w>=321) {
-		$('body').addClass('device');
-		Reveal.configure({ 
-			width: '620',
-			height: '1024'
-		});
-	} else if ( w <=320) {
-		$('body').addClass('device');
-		Reveal.configure({ 
-			width: '290',
-			height: '1024'
-		});
-	} else {
-		$('body').removeClass('device');
-		Reveal.configure({ 
-			width: '1920',
-			height:'1080'
-		});
+
+	if ( w > 1680) {
+		$('body').addClass('pc_large').removeClass('pc tablet phone');
+		hideNav();
+	} else if ( w <=1680 && w >1200) {
+		$('body').addClass('pc').removeClass('pc_large tablet phone');
+		hideNav();
+	} else if ( w <=1200 && w >=768) {
+		$('body').addClass('tablet').removeClass('pc pc_large phone');
+	} else if ( w <=768) {
+		$('body').addClass('phone').removeClass('pc tablet pc_large');
 	}
 }
 
@@ -58,9 +51,39 @@ var contactText = function(){
 	$('.home__contact input, .home__contact .textarea').focusout(function(){
 		if ( $(this).val().length == 0 ) {
 			$(this).parent().removeClass('act');
-		}
-		
+		}		
 	});	
+}
+
+/* burger */
+var burger = function(){
+	$('.burger').click(function(){
+
+		if ( $(this).hasClass('act') ) {
+			$(this).removeClass('act');
+		} else {
+			$(this).addClass('act');
+		}
+
+		if ( $('.nav_device').hasClass('act') ) {
+			$('.nav_device').removeClass('act');
+		} else {
+			$('.nav_device').addClass('act');
+		}
+
+		if ( $('.header .logo').hasClass('act') ) {
+			$('.header .logo').removeClass('act');
+		} else {
+			$('.header .logo').addClass('act');
+		}
+
+	});
+}
+
+var hideNav = function(){
+	$('.burger').removeClass('act');
+	$('.nav_device').removeClass('act');
+	$('.header .logo').removeClass('act');
 }
 
 $(document).ready(function(){
@@ -68,6 +91,10 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
+	$('.nav_device a').click(function(){
+		hideNav();
+	});
+	burger();
 	slideChange();
 	navFix();
 	sliderParallaxOnload();
